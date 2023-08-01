d0a5f15a1035a69a83a6d49a637da49517f29f0e
Regina <Regina3016@mail.ru>
2023-08-01 16:40:09 +0300

    приветствие любимчиков

---
+++ example5/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+    Console.WriteLine("УРА ЭТО ЖЕ МАША");
+}
+else
+{
+    Console.WriteLine("привет,");
+    Console.WriteLine(username);
+}
